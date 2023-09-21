a37d13116204ff65d7f4d6f008c9c94da4badcd3
Yupei Yang <yupei.yang@oracle.com>
2023-09-21 08:27:21 -0700

    update provider version

--- provider.tf
+++ provider.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     oci = {
       source  = "oracle/oci"
-      version = "4.96.0" # October 05, 2022 Release
+      version = "5.9.0" # October 05, 2022 Release
     }
   }
 }
