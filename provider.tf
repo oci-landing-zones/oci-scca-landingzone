dc5b0dfe76772f82b8f92d0f5c7e29b26175fdb8
Rory Nguyen <rory.nguyen@oracle.com>
2023-12-05 10:42:47 -0800

    added provider comment

--- provider.tf
+++ provider.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     oci = {
       source  = "oracle/oci"
-      version = "5.19.0" # August 2023 Release
+      version = "5.19.0" # November 2023 Release
     }
   }
 }
